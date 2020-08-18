8d5a670f4f95b9153e09d35dde281326e7097317
Christian Shaw <cxs.shaw@googlemail.com>
2020-08-18 21:13:51 +0100

    [mod] set correct path for stardog home var

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 
 if [ -f /mnt/stardog/stardog.properties ]; then
   echo "Setting stardog properties from mounted file"
-  cp /mnt/stardog/stardog.properties /opt/stardog/stardog.properties
+  cp /mnt/stardog/stardog.properties $STARDOG_HOME/stardog.properties
 else
   echo "No stardog properties found at mountpoint"
 fi
 
-printf '\n%s' "pack.node.address=$(hostname -f):5820" >> /opt/stardog/stardog.properties
+printf '\n%s' "pack.node.address=$(hostname -f):5820" >> $STARDOG_HOME/stardog.properties
 
 /opt/stardog/bin/stardog-admin server start --foreground --home $STARDOG_HOME --verbose
