abc7822db9728048fbd514a6d7b8ab4cddd3ed6b
Christian Shaw <cxs.shaw@googlemail.com>
2020-08-18 19:36:17 +0100

    [mod] debugging entrypoint

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -3,7 +3,10 @@
 set -e
 
 if [ -f /mnt/stardog/stardog.properties ]; then
+  echo "Setting stardog properties from mounted file"
   cp /mnt/stardog/stardog.properties /opt/stardog/stardog.properties
+else
+  echo "No stardog properties found at mountpoint"
 fi
 
 echo "pack.node.address=$(hostname -f):5280" >> /opt/stardog/stardog.properties
