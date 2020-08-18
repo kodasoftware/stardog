8bd0b9047086d31f85779f162d94b7bc3f6f1b52
Christian Shaw <cxs.shaw@googlemail.com>
2020-08-18 17:50:52 +0100

    [mod] mount properties and entrypoint

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ -f /mnt/stardog/stardog.properties ]; then
+  cp /mnt/stardog/stardog.properties /opt/stardog/stardog.properties
+fi
+
 echo "pack.node.address=$(hostname -f):5280" >> /opt/stardog/stardog.properties
 
 exec "$@"
